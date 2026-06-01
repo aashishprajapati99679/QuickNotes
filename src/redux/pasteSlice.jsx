c2f2723bd38da6617b1c013614946920031a4b6e
Ashish Prajapati <155898594+aashishprajapati99679@users.noreply.github.com>
2026-06-01 07:00:54 +0530

    Update pasteSlice.jsx

--- src/redux/pasteSlice.jsx
+++ src/redux/pasteSlice.jsx
@@ -16,16 +16,15 @@ const pasteSlice = createSlice({
       const index = state.pastes.findIndex((item) => item._id === paste._id)
 
       if (index >= 0) {
-        // If the course is already in the Pastes, do not modify the quantity
+      
         toast.error("Paste already exist")
         return
       }
-      // If the course is not in the Pastes, add it to the Pastes
+      
       state.pastes.push(paste)
       
-      // Update to localstorage
+      
       localStorage.setItem("pastes", JSON.stringify(state.pastes))
-      // show toast
       toast.success("Paste added")
     },
 
@@ -34,11 +33,8 @@ const pasteSlice = createSlice({
       const index = state.pastes.findIndex((item) => item._id === paste._id)
 
       if (index >= 0) {
-        // If the course is found in the Pastes, update it
         state.pastes[index] = paste
-        // Update to localstorage
         localStorage.setItem("pastes", JSON.stringify(state.pastes))
-        // show toast
         toast.success("Paste updated")
       }
     },
@@ -49,17 +45,17 @@ const pasteSlice = createSlice({
       const index = state.pastes.findIndex((item) => item._id === pasteId)
 
       if (index >= 0) {
-        // If the course is found in the Pastes, remove it
+        
         state.pastes.splice(index, 1)
-        // Update to localstorage
+        
         localStorage.setItem("pastes", JSON.stringify(state.pastes))
-        // show toast
+        
         toast.success("Paste deleted")
       }
     },
     resetPaste: (state) => {
       state.pastes = []
-      // Update to localstorage
+     
       localStorage.removeItem("pastes")
     },
   },
